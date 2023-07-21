83026b361f5c4cbfdf8ba90c8171fdaf9a5495d6
Célio Da Silva <celio01t@gmail.com>
2023-07-21 14:24:45 -0300

    FEAR : painel sendo criado

--- frontend/src/componentes/routes/Home.js
+++ frontend/src/componentes/routes/Home.js
@@ -1,9 +1,76 @@
 import style from "./Home.module.css";
 
+import Input from "../layout/Input";
+
 export default function Home() {
   return (
     <main className={style.main}>
-      <h1>home</h1>
+      <h1>Fluxo de caixa</h1>
+      <header>
+        <div>
+          <h1>ENTRADA</h1>
+          <span>
+            <h1> R$ +1000</h1>
+          </span>
+        </div>
+        <div>
+          <h1>SAÍDA</h1>
+          <span>
+            <h1>R$ -2000</h1>
+          </span>
+        </div>
+        <div>
+          <h1>CAIXA</h1>
+          <span>
+            <h1>R$ 3000</h1>
+          </span>
+        </div>
+      </header>
+
+      <form>
+        <Input
+          text="DATA"
+          placeholder="data"
+          type="date"
+          id="data"
+          name="data"
+          className={style.input}
+        />
+        <Input
+          text="MOVIMENTAÇÃO:"
+          placeholder="Entrada ou Saida"
+          type="text"
+          id="Movimentação"
+          name="Movimentação"
+          className={style.input}
+        />
+
+        <Input
+          text="DESCRIÇÃO:"
+          placeholder="Descrição"
+          type="text"
+          id="Descrição"
+          name="Descrição"
+          className={style.input}
+        />
+        <Input
+          text="VALOR:"
+          placeholder="valor"
+          type="number"
+          id="Valor"
+          name="Valor"
+          className={style.input}
+        />
+
+        <Input
+          text="Salvar"
+          placeholder="Salvar"
+          type="submit"
+          id="Salvar"
+          name="Salvar"
+          className={style.input}
+        />
+      </form>
     </main>
   );
 }
