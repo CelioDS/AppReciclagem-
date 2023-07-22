cb6a18ccedc92a41a2671a2e188ffabd614342d2
Célio Da Silva <celio01t@gmail.com>
2023-07-22 00:22:22 -0300

    FEAT : conexao bd

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "sql.freedb.tech",
-  user: "freedb_bussinessname",
-  password: "p2SNK9qUZW7!Vup",
-  database: "freedb_bussiness",
+  host: "db4free.net",
+  user: "teste01ada",
+  password: "PZXvAFdxa.bh2Yg",
+  database: "teste01ada",
 });
