526eb91012cde45c6973f118ca3a665d765897c3
Célio Da Silva <celio01t@gmail.com>
2023-07-26 20:33:51 -0300

    FEAT : Tela de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [user, setUser] = useState(localStorage.getItem("user") || "");
   const [pass, setPass] = useState(localStorage.getItem("pass") || "");
   const [permission, setPermission] = useState(false);
+  const [erro, seterro] = useState(false);
 
   const [zoomPage, setZoomPage] = useState(
     localStorage.getItem("FontSize") || 1
@@ -31,7 +32,9 @@ function App() {
     }
   }, [pass, user]);
 
-  function handleLogin() {
+  function handleLogin(e) {
+    e.preventDefault();
+
     if (
       user === process.env.REACT_APP_USER &&
       pass === process.env.REACT_APP_PASS
@@ -39,28 +42,36 @@ function App() {
       setPermission(true);
       localStorage.setItem("user", process.env.REACT_APP_USER);
       localStorage.setItem("pass", process.env.REACT_APP_PASS);
+    } else {
+      seterro(true);
+      setTimeout(() => {
+        seterro(false);
+      }, 3000);
+
+      toast.error("Credencias invalida!!!");
     }
   }
   return (
     <div className="App" style={{ zoom: zoomPage }}>
-      <NavBar />
+      {permission && <NavBar />}
       {permission && <Outlet />}
       {!permission && (
-        <form className="login-form">
+        <form className={`login-form ${erro === true ? "erro" : ""}`}>
           <h2>Login</h2>
+          <br />
           <input
             type="text"
             placeholder="Usuário"
             value={user}
             onChange={(e) => setUser(e.target.value)}
-            className="login-input"
+            className={`login-input ${erro === true ? "erro" : ""}`}
           />
           <input
             type="password"
             placeholder="Senha"
             value={pass}
             onChange={(e) => setPass(e.target.value)}
-            className="login-input"
+            className={`login-input ${erro === true ? "erro" : ""}`}
           />
           <button
             onClick={handleLogin}
@@ -69,9 +80,9 @@ function App() {
           >
             Entrar
           </button>
+          {erro && <h6>Nâo é um terremoto e o Usuario/Senha errada!</h6>}
         </form>
       )}
-
       <ToastContainer
         pauseOnHover={false}
         autoClose={3000}
